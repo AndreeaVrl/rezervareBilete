7cf37495a76f2bf8cbe4ee801ea4f57384629253
AndreeaVrl <virlan.andreea95@yahoo.com>
2017-12-10 09:49:06 +0200

    push TaraDao and interface

---
+++ RezervareBilet/src/main/java/it/rezervare/beans/dao/Interfaces/ITaraDAO.java
@@ -0,0 +1,14 @@
+package it.rezervare.beans.dao.Interfaces;
+
+import it.rezervare.beans.model.Localitate;
+import it.rezervare.beans.model.Tara;
+
+public interface ITaraDAO {
+
+	public void inserTara(Tara tara);
+
+	Tara getTaraByDenumire(String denumire);
+
+	void insertLocalitate(Localitate localitate);
+
+}
