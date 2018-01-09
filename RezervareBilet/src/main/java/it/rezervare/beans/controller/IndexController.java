6df540093aa90472ea09141eb7827b5088f2d947
Nina <Nina@Nina-PC>
2018-01-09 02:04:58 +0200

    chooseCountry

--- RezervareBilet/src/main/java/it/rezervare/beans/controller/IndexController.java
+++ RezervareBilet/src/main/java/it/rezervare/beans/controller/IndexController.java
@@ -1,14 +1,25 @@
 package it.rezervare.beans.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
+import it.rezervare.beans.helper.helperinterface.IFromToHelper;
+import it.rezervare.beans.model.hibernateBeans.Tara;
 import it.rezervare.beans.model.requestBeans.CursaRequestView;
 
 @Controller
 public class IndexController {
+	private IFromToHelper fromToRoute;
+	
+	@Autowired
+	public IndexController(IFromToHelper fromToRoute){
+		this.fromToRoute = fromToRoute;
+	}
 	
 	@RequestMapping(value = { "/"}, method = {RequestMethod.GET})
 	public ModelAndView login(ModelAndView model){
@@ -24,4 +35,10 @@ public class IndexController {
 		model.setViewName("admin");
 		return model;
 	}
+	
+	@RequestMapping(value = { "/index"}, method = {RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView chooseCountry(ModelAndView model, @ModelAttribute("tara") Tara tara, HttpServletRequest request){
+		return fromToRoute.chooseCountry(model,tara, request);
+	}
 }
+	
